24152b461e98129fdc5c5749f602454439fb35f9
Roberto Carlos Franco Tenorio <robertofrancotenorio2@gmail.com>
2017-08-24 10:44:17 -0500

    El programa pide que ingreses el nombre de el libro y de el autor

---
+++ Libro.java
@@ -0,0 +1,11 @@
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String nombre, String escritor)
+    {
+        titulo=nombre;
+        autor=escritor;
+    }
+}
